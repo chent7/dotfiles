8df583ced9d09c24c42ff116abb9d2d356563d84
-pacer- <chent7@outlook.com>
2021-10-07 20:16:59 +0800

    Added nm-applet for autostarting, updated polybar config for bspwm

--- .scripts/polybar.sh
+++ .scripts/polybar.sh
@@ -3,12 +3,28 @@
 killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# source pywal colors
-source ~/.cache/wal/colors.sh
-
-# export envar with alpha set
+# export envar for pywal colors instead of sourcing 
+# so colors hold after reboot
 ALPHA=aa;
-export background_a="#$ALPHA${background/'#'}"
+export background="${background}"
+export background_alpha="#$ALPHA${background/'#'}"
+export foreground="${foreground}"
+export color0="${color0}"
+export color1="${color1}"
+export color2="${color2}"
+export color3="${color3}"
+export color4="${color4}"
+export color5="${color5}"
+export color6="${color6}"
+export color7="${color7}"
+export color8="${color8}"
+export color9="${color9}"
+export color10="${color10}"
+export color11="${color11}"
+export color12="${color12}"
+export color13="${color13}"
+export color14="${color14}"
+export color15="${color15}"
 
 # launch polybar
 polybar $1
