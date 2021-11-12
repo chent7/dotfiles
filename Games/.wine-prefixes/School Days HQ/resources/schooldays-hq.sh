8b4f885449d7260638518d4a5ba2f716d89b3393
-pacer- <chent7@outlook.com>
2021-11-12 22:20:39 +0800

    changed directory structure for game configs: replaced /home/pacer with /home/pacer in scripts

--- Games/.wine-prefixes/School Days HQ/resources/schooldays-hq.sh
+++ Games/.wine-prefixes/School Days HQ/resources/schooldays-hq.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export WINEPREFIX="$HOME/Games/School Days HQ/.config/wine"
+export WINEPREFIX="$HOME/Games/.wine-prefixes/School Days HQ/wine"
 wine "$HOME/Games/School Days HQ/Overflow/SCHOOLDAYS HQ/SCHOOLDAYS HQ.exe"
--- Games/.wine-prefixes/Shiny Days/resources/shinydays.sh
+++ Games/.wine-prefixes/Shiny Days/resources/shinydays.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export WINEPREFIX="$HOME/Games/Shiny Days/.config/wine"
+export WINEPREFIX="$HOME/Games/.wine-prefixes/Shiny Days/wine"
 wine "$HOME/Games/Shiny Days/Overflow/SHINYDAYS/SHINYDAYS.exe"
